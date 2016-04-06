93c6b636d5325e829cc44a35b6a5a2ef003c5305
godka <asdic182@sina.com>
2016-04-06 21:17:00 +0800

    学校机器太废了，搞成三线程

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ namespace pinkspider
             if (args.Length > 0)
             {
                 var strs = System.IO.File.ReadAllLines(args[0], System.Text.Encoding.UTF8);
-                spider = new MythSpiderPool(strs, 4);
+                spider = new MythSpiderPool(strs, 3);
             }
             else
             {
-                spider = new MythSpiderPool("http://www.iqiyi.com", 4);
+                spider = new MythSpiderPool("http://www.iqiyi.com", 3);
             }
             spider.StartLoop();
             //Console.ReadKey ();
